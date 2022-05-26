9dfc6d3d7e607146f91f4301759da268fc228b3a
benzkittisak <benzkittisak@stv.ac.th>
2022-05-26 18:09:06 +0700

    update code and update version of dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import * as esbuild from "esbuild-wasm";
 
 const App: React.FC = () => {
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
 
   // output from esbuild
   const [code, setCode] = useState("");
 
-  const onClick = () => {
-    console.log(input);
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) return;
+    const result = await ref.current.transform(input, {
+      loader: "jsx",
+      target: "es2015",
+    });
+
+    setCode(result.code);
   };
 
   return (
     <>
-      <textarea value={input} cols={80} rows={20} onChange={(e) => setInput(e.target.value)}>
+      <textarea
+        value={input}
+        cols={80}
+        rows={20}
+        onChange={(e) => setInput(e.target.value)}
+      >
         {input}
       </textarea>
       <div>
-        <button onClick={onClick}>Submit</button>
+        <button type="button" onClick={onClick}>
+          Submit
+        </button>
       </div>
-      <pre>{code}</pre>
+      <pre ref={ref}>{code}</pre>
     </>
   );
 };
