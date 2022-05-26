80661b20addf830ea21d7e69d3a1d0408a0abf11
benzkittisak <benzkittisak@stv.ac.th>
2022-05-26 12:30:30 +0700

    update code and move esbuild.wasm to public directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App: React.FC = () => {
+  const [input, setInput] = useState("");
+
+  // output from esbuild
+  const [code, setCode] = useState("");
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <textarea value={input} cols={80} rows={20} onChange={(e) => setInput(e.target.value)}>
+        {input}
+      </textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </>
   );
-}
+};
 
 export default App;
