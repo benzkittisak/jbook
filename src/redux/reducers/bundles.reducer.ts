6617dc2b758714cd3d7c4453e15952d80d8815e0
benzkittisak <benzkittisak@stv.ac.th>
2022-06-04 09:45:56 +0700

    update bundle reducer

--- src/redux/reducers/bundles.reducer.ts
+++ src/redux/reducers/bundles.reducer.ts
@@ -1,1 +1,27 @@
-export default 1;
+import produce from "immer";
+
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundleState {
+  [key: string]: {
+    loading: boolean;
+    code: string;
+    err: string;
+  };
+}
+
+const initialState: BundleState = {};
+
+const bundleReducer = produce((state: BundleState = initialState , action: Action):BundleState =>{
+    switch(action.type){
+        case ActionType.BUNDLE_START:
+            return state;
+        case ActionType.BUNDLE_COMPLETE:
+            return state;
+        default:
+            return state;
+    }
+})
+
+export default bundleReducer;
