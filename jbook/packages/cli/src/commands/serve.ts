2d856c651594c28fd8a25d971ea83b6ce2e07600
benzkittisak <benzkittisak@stv.ac.th>
2022-06-05 12:24:22 +0700

    update cli code

--- jbook/packages/cli/src/commands/serve.ts
+++ jbook/packages/cli/src/commands/serve.ts
@@ -6,7 +6,22 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p , --port <number>", "port to run the server on ", "4005")
-  .action((filename = "notebook.js", options: { port: string }) => {
-    const dir = path.join(process.cwd(), path.dirname(filename));
-    serve(parseInt(options.port), path.basename(filename), dir);
+  .action(async (filename = "notebook.js", options: { port: string }) => {
+    try {
+      const dir = path.join(process.cwd(), path.dirname(filename));
+      await serve(parseInt(options.port), path.basename(filename), dir);
+      console.log(
+        `Opened ${filename}. Navigate to http://localhost:${options.port} to edit`
+      );
+    } catch (error) {
+      // console.log(error.messsage);
+      if (error instanceof Error) {
+        if (error.name === "EADDRINUSE") {
+          console.error("Port is in use. Try running on a different port");
+        }
+      } else {
+        console.log("Here is the error message", error);
+      }
+      process.exit(1);
+    }
   });
