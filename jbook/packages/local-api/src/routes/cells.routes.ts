5368bcd4eabffdf88faf9ff9e1c7be5d30834ed9
benzkittisak <benzkittisak@stv.ac.th>
2022-06-05 14:51:00 +0700

    update routes in local-api

--- jbook/packages/local-api/src/routes/cells.routes.ts
+++ jbook/packages/local-api/src/routes/cells.routes.ts
@@ -1,24 +1,53 @@
 import express from "express";
 // file storage manager
-import fs from 'fs/promises';
+import fs from "fs/promises";
+import path from "path";
+
+interface Cell {
+  id: string;
+  content: string;
+  type: "text" | "code";
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
+  router.use(express.json());
+  
+  const fullPath = path.join(dir, filename);
 
-  router.get("/cells", async () => {
-    // Make sure the cell storage is exists
-    // If it does not exist,  add in a default list of cells
-    // Read the file
-    // Parse a list of cells out of it
-    // Send list of cells back to the browser
+  router.get("/cells", async (req, res) => {
+    try {
+      // Make sure the cell storage is exists
+      // If it does not exist,  add in a default list of cells
+      // Read the file
+
+      const result = await fs.readFile(fullPath, { encoding: "utf8" });
+      res.send(JSON.parse(result));
+    } catch (error) {
+      //  If red throw an error
+      // Inspect the error, see if it says that the file doesn't exist'
+      // Parse a list of cells out of it
+      // Send list of cells back to the browser
+      if (error instanceof Error) {
+        if (error.name === "ENOENT") {
+          await fs.writeFile(fullPath, "[]", "utf-8");
+          res.json([]);
+        } else {
+          throw error;
+        }
+      }
+    }
   });
 
-  router.post("/cells", async () => {
-    // Make sure the file exists
-    // If not, create it
+  router.post("/cells", async (req, res) => {
     // Take the list of cells from the request object
     // serialize them
+    // Array of objects
+    const { cells }: { cells: Cell[] } = req.body;
+
     // Write the cells into the file
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf-8");
+    res.send({ state: "ok" });
   });
 
   return router;
